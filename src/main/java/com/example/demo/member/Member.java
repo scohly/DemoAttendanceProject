0986914dd594ae7b285e0bdd5fbbfeaa6b6f0fc8
scohly <scohly@fordham.edu>
2021-06-06 23:45:30 -0400

    added properties file, and model member package

---
+++ src/main/java/com/example/demo/member/Member.java
@@ -0,0 +1,97 @@
+package com.example.demo.member;
+
+import java.time.LocalDate;
+
+//this member class is basically our member object
+//will have id, name, attendance date, integer age, UID (db number)
+public class Member {
+    private Long id;
+    private String name;
+    private String email;
+    private String uid; //unique id number given by DB
+    private Integer age;
+    private LocalDate attendanceDate;
+
+    public Member() {
+    }
+
+    //constsructor w/o ID as DB will create id
+    public Member(String name, String email, String uid, Integer age, LocalDate attendanceDate) {
+        this.name = name;
+        this.email = email;
+        this.uid = uid;
+        this.age = age;
+        this.attendanceDate = attendanceDate;
+    }
+
+    public Member(Long id, String name, String email, String uid, Integer age, LocalDate attendanceDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.uid = uid;
+        this.age = age;
+        this.attendanceDate = attendanceDate;
+    }
+
+    //getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getAttendanceDate() {
+        return attendanceDate;
+    }
+
+    public void setAttendanceDate(LocalDate attendanceDate) {
+        this.attendanceDate = attendanceDate;
+    }
+
+    @Override
+    public String  toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", uid='" + uid + '\'' +
+                ", age=" + age +
+                ", attendanceDate=" + attendanceDate +
+                '}';
+    }
+}
